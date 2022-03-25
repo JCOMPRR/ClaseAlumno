fc350d3f8c9eb7391d740c4d795c7de4581afe14
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-03-25 15:17:23 -0700

    Cambio de datos a los alumnos y se agrego Alumno3

--- ClaseAlumno/Program.cs
+++ ClaseAlumno/Program.cs
@@ -3,20 +3,27 @@
 List<Alumno> lista = new List<Alumno>();
 
 Alumno alumno1 = new Alumno();
-alumno1.Nombres = "Antonio";
-alumno1.ApellidoPaterno = "Rogel";
-alumno1.Matricula = "123";
-alumno1.FechaNacimiento = DateTime.Now;
+alumno1.Nombres = "Hector";
+alumno1.ApellidoPaterno = "Favela";
+alumno1.Matricula = "01234";
+alumno1.FechaNacimiento = DateTime(2001, 11, 23);
 
 
 Alumno alumno2 = new Alumno();
-alumno2.Nombres = "Juan";
-alumno2.ApellidoPaterno = "Lopez";
-alumno2.Matricula = "456";
+alumno2.Nombres = "Luis";
+alumno2.ApellidoPaterno = "Ramirez";
+alumno2.Matricula = "56789";
 alumno2.FechaNacimiento = new DateTime(1980, 02, 28);
+
+Alumno alumno3 = new Alumno();
+alumno2.Nombres = "Jose";
+alumno2.ApellidoPaterno = "Rodriguez";
+alumno2.Matricula = "56789";
+alumno2.FechaNacimiento = new DateTime(2002, 03, 15);
 
 lista.Add(alumno1);
 lista.Add(alumno2);
+lista.Add(alumno3);
 
 foreach (Alumno alumnoEnLista in lista)
 {
