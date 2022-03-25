08139bed033394ec6d03a4fa2842e7fbfc0e84ad
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-03-25 15:10:25 -0700

    Agregar archivos de proyecto.

---
+++ ClaseAlumno/Alumno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAlumno
+{
+    internal class Alumno
+    {
+    }
+}
